88cd12d69304beb32f8bea55bc0235554170a69b
Seyed Danial Movahed <Movaheddanial@gmail.com>
2022-01-06 07:05:39 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 mkdir -p /home/$USER/.local/bin
 cp mutefixapi /home/$USER/.local/bin
 cp muteledctl.py /home/$USER/.local/bin/muteledctl
-echo "/home/${USER}/.local/bin/muteledctl" >> /home/$USER/.profile
+echo "/home/${USER}/.local/bin/muteledctl &" >> /home/$USER/.profile
 echo "${USER} ALL=(ALL) NOPASSWD: /home/${USER}/.local/bin/mutefixapi" | pkexec env EDITOR="tee -a" visudo
 chmod +x /home/$USER/.local/bin/muteledctl
 /home/$USER/.local/bin/muteledctl &
