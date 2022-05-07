95efa71b4ad643f43bd961432e49966e1df68899
Seyed Danial Movahed <Movaheddanial@gmail.com>
2022-05-07 16:08:21 +0430

    Fixed a typo

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ cp muteledctl.py /home/$USER/.local/bin/muteledctl
 echo "/home/${USER}/.local/bin/muteledctl &" >> /home/$USER/.profile
 echo "${USER} ALL=(ALL) NOPASSWD: /home/${USER}/.local/bin/mutefixapi" | pkexec env EDITOR="tee -a" visudo
 chmod +x /home/$USER/.local/bin/muteledctl
-chmod +x /home/arch/.local/bin/mutefixapi
+chmod +x /home/$USER/.local/bin/mutefixapi
 /home/$USER/.local/bin/muteledctl &
 notify-send "MSI Keyboard led fix has been installed!"
